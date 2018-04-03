58ca90927f44f90ea1f1ef851cdb56cf7070fa43
bill <bill@ubill-HP>
2018-04-03 16:33:05 +0300

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,16 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Apr 3, 2018
+ *      Author: bill
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    cout << "Hello World!" << endl;
+}
